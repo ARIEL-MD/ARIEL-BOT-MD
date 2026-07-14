abd1e1805fad3a9f0faa28b5ba106172def149f0
ARIEL-MD <kouameariel79@gmail.com>
2026-07-14 08:37:52 +0300

    Update index.js

--- commands/index.js
+++ commands/index.js
@@ -2800,11 +2800,6 @@ commands.push(...wallpaperCommands);
 const bibleCommands = require("./bible");
 commands.push(...bibleCommands);
 
-// ----- Commande ajoutée : xnxx (.xnxx) — voir
-// commands/xnxx.js -----
-const xnxxCommands = require("./xnxx");
-commands.push(...xnxxCommands);
-
 module.exports = commands;
 module.exports.getMenuImageBuffer = getMenuImageBuffer;
 module.exports.initScheduledStatuses = profileStatusCommands.initScheduledStatuses;
